c837c8ceea8c0ba0fec23faa38c97c1cb87c116d
Miguel <cairesmontalvaomiguel@gmail.com>
2024-05-08 22:06:28 -0300

    08/05/2024 - Modulação e Organização

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      teste
+      Hello World!!
   </React.StrictMode>
 );
 
